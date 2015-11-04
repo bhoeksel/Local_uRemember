249cc5d865fb3ee2836fb27ac4abdee3f76fcdf8
bhoeksel <hoekselstuff@gmail.com>
2015-11-04 16:44:05 -0700

    Added Second Screen, button push works.

--- app/src/main/java/com/example/bhoeksel/local_uremember/MainActivity.java
+++ app/src/main/java/com/example/bhoeksel/local_uremember/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.bhoeksel.local_uremember;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mainListView;
+    private ArrayAdapter<String> listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +40,13 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, Choose_Deck.class);
+        startActivity(intent);
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
